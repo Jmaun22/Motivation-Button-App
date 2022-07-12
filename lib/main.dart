4737c9f809599b2c4f03c750e60768c737b4b2d9
Jesse Maun <jesse@Jesses-MacBook-Air.local>
2022-07-12 10:10:28 -0400

    added and refactored main dart to get the sound to work when you click on a button

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,48 @@
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:audioplayers/audio_cache.dart';
 import 'dart:math';
 
-void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          title: Text('You got this'),
-          backgroundColor: Color.fromARGB(255, 41, 141, 223),
-        ),
-        body: ButtonPage(),
+void main() => runApp(MotivationApp());
+
+class MotivationApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('quote$soundNumber.wav');
+  }
+
+  Expanded buildKey({Color color, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+             soundNumber = Random().nextInt(6) + 1;
+          playSound(soundNumber);
+        },
+        child: Image.asset('assets/button.png'),
       ),
-    ),
-  );
-}
+    );
+  }
 
-class ButtonPage extends StatefulWidget {
-  const ButtonPage({Key? key}) : super(key: key);
-
-  @override
-  State<ButtonPage> createState() => _ButtonPageState();
-}
-
-class _ButtonPageState extends State<ButtonPage> {
-  int leftDiceNumber = 1;
-
-  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FlatButton(
-                  onPressed: () {
-                    setState(() {
-                      leftDiceNumber = Random().nextInt(6) + 1;
-                    });
-                  },
-                  child: Image.asset('images/button.png'),
-                  ),
-            ),
+    return MaterialApp(
+      home: Scaffold(
+         backgroundColor: Colors.blue,
+           appBar: AppBar(
+          title: Text('You got this!!'),
+          backgroundColor: Color.fromARGB(255, 41, 141, 223),
+        ),
+        body: SafeArea(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                buildKey(color: Colors.blue, soundNumber: 1),
+              
+             
+              ],
           ),
-          
-        ],
+        ),
       ),
     );
   }
