f59093d93d5ad3e5730e15cf8b7ce0e1b5a03c92
Jesse Maun <jesse@Jesses-MacBook-Air.local>
2022-07-12 09:26:34 -0400

    added sounds file

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
         backgroundColor: Colors.blue,
         appBar: AppBar(
           title: Text('You got this'),
-          backgroundColor: Colors.blue,
+          backgroundColor: Color.fromARGB(255, 41, 141, 223),
         ),
         body: ButtonPage(),
       ),
@@ -24,7 +24,7 @@ class ButtonPage extends StatefulWidget {
 }
 
 class _ButtonPageState extends State<ButtonPage> {
-    int leftDiceNumber = 1;
+  int leftDiceNumber = 1;
 
   int rightDiceNumber = 1;
   @override
@@ -41,22 +41,11 @@ class _ButtonPageState extends State<ButtonPage> {
                       leftDiceNumber = Random().nextInt(6) + 1;
                     });
                   },
-                  child: Image.asset('images/dice$leftDiceNumber.png')),
+                  child: Image.asset('images/button.png'),
+                  ),
             ),
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FlatButton(
-                onPressed: () {
-                  setState(() {
-                    rightDiceNumber = Random().nextInt(6) + 1;
-                  });
-                },
-                child: Image.asset('images/dice$rightDiceNumber.png'),
-              ),
-            ),
-          ),
+          
         ],
       ),
     );
